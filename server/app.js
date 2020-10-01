76ff3aaed0ddf44e9fde0df046b623e5b39e87f8
roydamari <roydamari92@gmail.com>
2020-10-01 18:35:25 +0300

    get_post_delete
    basic get, post and delete enry points

--- server/app.js
+++ server/app.js
@@ -4,4 +4,25 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/messages')
+const messages = [];
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body.message);
+    res.send('sent');
+})
+
+app.delete('/message/:id', (req, res) => {
+    if (req.params.id < messages.length) {
+        messages.splice(req.params.id, 1);
+        res.json(messages);
+    } else {
+        res.send('message doesn\'t exist');
+    }
+})
+
+
+module.exports = app;
